eb503ffcc4b0d54fa6913951a59e4fab91353f76
bloquer <995skh@gmail.com>
2018-12-05 12:15:52 +0900

    init

--- proto/proto/view_floor.cpp
+++ proto/proto/view_floor.cpp
@@ -55,7 +55,7 @@ bool eyetop = false;
 double speed = 2.0;
 
 double eye_x = 0.0;
-double eye_y = 20.0;
+double eye_y = 10.0;
 double eye_z = 0.0;
 
 double at_x = 0.0;
@@ -69,7 +69,7 @@ double initAt_z = 0.0;
 double rl_angle = 0.0;
 double ud_angle = 0.0;
 
-double r = 100;
+double r = 10;
 double updown = 0;
 double theta = 1.5;
 int mouse_x = 0;
@@ -79,12 +79,51 @@ cube map_item[ITEMMAX];
 int item_id[ITEMMAX] = { 0, };
 int itemcount = 0;
 int id = 0;
+GLuint texture; //the array for our texture
 
 int framebufferWidth, framebufferHeight;
 GLuint triangleVertexArrayObject;
 GLuint triangleShaderProgramID;
 GLuint trianglePositionVertexBufferObjectID, triangleColorVertexBufferObjectID;
 GLuint triangleTextureCoordinateBufferObjectID;
+
+GLuint LoadTexture(const char * filename, int width, int
+	height)
+{
+	GLuint texture;
+	unsigned char * data;
+	FILE * file;
+
+	fopen_s(&file, filename, "rb");
+	if (file == NULL) return 0;
+	data = (unsigned char *)malloc(width * height * 3);
+	fread(data, width * height * 3, 1, file);
+	fclose(file);
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE,
+		GL_MODULATE);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
+		GL_NEAREST);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,
+		GL_NEAREST);
+
+
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S,
+		GL_REPEAT);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T,
+		GL_REPEAT);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0,
+		GL_RGB, GL_UNSIGNED_BYTE, data);
+	free(data);
+	return texture;
+}
+
+void FreeTexture(GLuint texture)
+{
+	glDeleteTextures(1, &texture);
+}
 
 int main(int argc, char **argv)
 {
@@ -163,7 +202,8 @@ void reshape(int width, int height)
 	glViewport(0, 0, width, height);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluPerspective(45, 2, 100, 2000);
+	glFrustum(-20, 20, 5, 25, 100, 200);
+	// gluPerspective(45, 2, 100, 200);
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 }
@@ -231,8 +271,8 @@ void keyboard(unsigned char key, int x, int y)
 		break;
 	case 'h':
 		// 시점에 대해 큐브 중점의 x, z 결정
-		map_item[itemcount].x = ((int)at_x / 50) * 50 + 25;
-		map_item[itemcount].z = ((int)at_z / 50) * 50 + 25;
+		map_item[itemcount].x = ((int)at_x / 10) * 10 + 5;
+		map_item[itemcount].z = ((int)at_z / 10) * 10 + 5;
 		// 첫번째 아이템에 대한 작업
 		if (itemcount == 0)
 		{
@@ -252,7 +292,7 @@ void keyboard(unsigned char key, int x, int y)
 				if (map_item[itemcount].x == map_item[i].x && map_item[itemcount].z == map_item[i].z)
 				{
 					// 해당 y축을 아이템 갯수만큼 증가
-					map_item[itemcount].y = item_id[i] * 50 - 40;
+					map_item[itemcount].y = item_id[i] * 10 - 40;
 					map_item[itemcount].id = i;
 					map_item[itemcount].exist = true;
 					// 개수 증가
@@ -274,8 +314,8 @@ void keyboard(unsigned char key, int x, int y)
 		}
 		break;
 	case 'r':
-		temp.x = ((int)at_x / 50) * 50 + 25;
-		temp.z = ((int)at_z / 50) * 50 + 25;
+		temp.x = ((int)at_x / 10) * 10 + 5;
+		temp.z = ((int)at_z / 10) * 10 + 5;
 		for (int i = 0; i <= itemcount; i++)
 		{
 			if (temp.x == map_item[i].x && temp.z == map_item[i].z)
@@ -331,19 +371,19 @@ void keyboard(unsigned char key, int x, int y)
 		if (eyetop == false)
 		{
 			eye_y += 1.0;
-			if (eye_y >= 29.0)
+			if (eye_y >= 19.0)
 				eyetop = true;
 		}
 		else
 		{
 			eye_y -= 1.0;
-			if (eye_y <= 20.0)
+			if (eye_y <= 10.0)
 				eyetop = false;
 		}
 	}
 	else if (run_state == true)
 	{
-		eye_y = 20.0;
+		eye_y = 10.0;
 	}
 
 	glutPostRedisplay();
@@ -379,7 +419,7 @@ void moveCamera()
 void init()
 {
 	initAt_x = r * cos(0.0);
-	initAt_y = 20.0;
+	initAt_y = 10.0;
 	initAt_z = r * sin(0.0);
 
 	at_x = initAt_x;
@@ -394,8 +434,8 @@ void mkList()
 
 	glColor3ub(255, 0, 0);
 	glPushMatrix();
-	glTranslatef(400, 0, 0);
-	glutSolidCube(50);
+	glTranslatef(150, 0, 0);
+	glutSolidCube(10);
 	glPopMatrix();
 
 	glEndList();
@@ -405,21 +445,21 @@ void mkList()
 	glCallList(idInnerList);
 
 	glPushMatrix();
-	glTranslatef(0, 0, -200);
+	glTranslatef(0, 0, -50);
 	glCallList(idInnerList);
 	glPopMatrix();
 	glEndList();
 
-	for (int i = 0; i < 20; i++)
+	for (int i = 0; i < 100; i++)
 	{
-		for (int j = 0; j < 20; j++)
+		for (int j = 0; j < 100; j++)
 		{
 			glColor3ub(0, 255, 0);
 			glPushMatrix();
-			glTranslatef(i * 50 - 200, -90, -200);
-			glTranslatef(-200, 0, j * 50 - 200);
-			glutSolidCube(50);
-			glTranslatef((i * -50) - 200, 90, (j * -50) - 200);
+			glTranslatef(i * 10 - 100, -5, -100);
+			glTranslatef(-100, 0, j * 10 - 100);
+			glutSolidCube(10);
+			glTranslatef((i * -10) - 100, 5, (j * -10) - 200);
 			glPopMatrix();
 		}
 	}
@@ -476,7 +516,7 @@ void DrawScene()
 	glPushMatrix();
 	glTranslatef(0.0, 0.0, 1.6);
 	glBegin(GL_POLYGON);
-	glColor3ub(0, 0, 0);
+	glColor3ub(255, 255, 255);
 	glVertex3f(-1.0, 2.0, 1.0);
 	glVertex3f(-1.0, 2.0, -1.0);
 	glVertex3f(-1.0, -2.0, -1.0);
